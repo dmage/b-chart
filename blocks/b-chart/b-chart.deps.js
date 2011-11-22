ae7015ce891e463547e4160f3b3c4d8f5282c7f8
Oleg Bulatov <oleg@bulatov.mobi>
2011-11-22 16:34:45 +0400

    Add style for elem .b-chart__title
    Signed-off-by: Oleg Bulatov <oleg@bulatov.mobi>

--- blocks/b-chart/b-chart.deps.js
+++ blocks/b-chart/b-chart.deps.js
@@ -1,11 +1,12 @@
 ({
     shouldDeps: [
-        { elem: 'static-settings-provider' },
+        { elem: 'title' },
         { elem: 'axis' },
-        { block: 'b-busy' },
-        { block: 'b-scale', elem: 'linear' },
-        { block: 'b-chart-overlay', elem: 'grid' },
         { block: 'b-axis' },
+        { block: 'b-busy' },
+        { block: 'b-chart-overlay', elem: 'grid' },
+        { elem: 'static-settings-provider' },
+        { block: 'b-scale', elem: 'linear' },
     ],
     mustDeps: [
         {

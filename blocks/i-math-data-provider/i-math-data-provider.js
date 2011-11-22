793ffe3a1e29e2c4a9996449ebf2c4eb4a862a7a
Oleg Bulatov <oleg@bulatov.mobi>
2011-11-22 18:37:27 +0400

    Add i-math-data-provider
    Signed-off-by: Oleg Bulatov <oleg@bulatov.mobi>

---
+++ blocks/i-math-data-provider/i-math-data-provider.js
@@ -0,0 +1,32 @@
+/** @requires BEM */
+
+(function() {
+
+BEM.decl('i-math-data-provider', {
+
+    get : function(xbegin, xend) {
+        var step = this.params.step || 1;
+        var f = this[this.params.func || 'sin'] || this.sin;
+
+        var x = Math.ceil(xbegin / step) * step;
+        var i = 0;
+        var length = Math.floor((xend - x) / step);
+        var xData = new Array(length);
+        var yData = new Array(length);
+        while (x < xend) {
+            xData[i] = x;
+            yData[i] = f(x);
+            x += step;
+            i += 1;
+        }
+        return {
+            xData: xData,
+            yData: yData
+        };
+    },
+
+    sin : Math.sin
+
+});
+
+})();
